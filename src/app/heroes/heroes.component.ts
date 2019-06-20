99324727effa82d1ad410b69f72e95642ee1d1a8
GillesLB <deltag@laposte.net>
2019-06-20 17:17:50 +0200

    fin 2eme Displaying a data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1, 
-    name: 'Captain POD'
-  };
+  heroes = HEROES;
+
+  selectedHero: Hero;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
